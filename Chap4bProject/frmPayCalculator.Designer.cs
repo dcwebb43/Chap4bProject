de4a11eeb80b833ea1b08a9b3364c2fd8a38d355
Dalyssa <Dalyssa@DESKTOP-QJUVPV9>
2019-03-07 16:04:01 -0600

    Add project files.

---
+++ Chap4bProject/frmPayCalculator.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Chap4bProject
+{
+    partial class frmPayCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtSales = new System.Windows.Forms.TextBox();
+            this.txtAdvancePay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCommissionRate = new System.Windows.Forms.Label();
+            this.lblCommission = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblNetPay = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(165, 48);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(105, 20);
+            this.txtLastName.TabIndex = 0;
+            // 
+            // txtSales
+            // 
+            this.txtSales.Location = new System.Drawing.Point(165, 84);
+            this.txtSales.Name = "txtSales";
+            this.txtSales.Size = new System.Drawing.Size(74, 20);
+            this.txtSales.TabIndex = 1;
+            // 
+            // txtAdvancePay
+            // 
+            this.txtAdvancePay.Location = new System.Drawing.Point(165, 121);
+            this.txtAdvancePay.Name = "txtAdvancePay";
+            this.txtAdvancePay.Size = new System.Drawing.Size(74, 20);
+            this.txtAdvancePay.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Last Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sales for Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Advance Pay Received";
+            // 
+            // lblCommissionRate
+            // 
+            this.lblCommissionRate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCommissionRate.Location = new System.Drawing.Point(165, 178);
+            this.lblCommissionRate.Name = "lblCommissionRate";
+            this.lblCommissionRate.Size = new System.Drawing.Size(57, 20);
+            this.lblCommissionRate.TabIndex = 6;
+            // 
+            // lblCommission
+            // 
+            this.lblCommission.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCommission.Location = new System.Drawing.Point(165, 212);
+            this.lblCommission.Name = "lblCommission";
+            this.lblCommission.Size = new System.Drawing.Size(74, 20);
+            this.lblCommission.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Commission Rate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(97, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Commission";
+            // 
+            // lblNetPay
+            // 
+            this.lblNetPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNetPay.Location = new System.Drawing.Point(165, 244);
+            this.lblNetPay.Name = "lblNetPay";
+            this.lblNetPay.Size = new System.Drawing.Size(74, 20);
+            this.lblNetPay.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Net Pay";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(38, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(275, 24);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Commission and Pay Calculator";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(19, 298);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(74, 34);
+            this.btnCalculate.TabIndex = 13;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(133, 298);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(74, 34);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(247, 298);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(74, 34);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmPayCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 344);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblNetPay);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblCommission);
+            this.Controls.Add(this.lblCommissionRate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdvancePay);
+            this.Controls.Add(this.txtSales);
+            this.Controls.Add(this.txtLastName);
+            this.Name = "frmPayCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DS3850: Chapter 4b Project";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPayCalculator_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtSales;
+        private System.Windows.Forms.TextBox txtAdvancePay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCommissionRate;
+        private System.Windows.Forms.Label lblCommission;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblNetPay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Chap4bProject/frmPayCalculator.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* Commission rate information:
+ *  If monthlySales < 10000 Then rate = .05
+ *  If monthlySales 10000 to 14999 Then rate = .1
+ *  If monthlySales 15000 to 17999 Then rate = .12
+ *  If monthlySales 18000 to 21999 Then rate = .14
+ *  If monthlySales >= 22000 Then rate = .15
+ */
+
+namespace Chap4bProject
+{
+    public partial class frmPayCalculator : Form
+    {
+        public frmPayCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare variables and constants
+            decimal decSales, decCommissionRate, decCommission, decNetPay;
+            int intAdvPay;
+            Boolean blnValid = true;
+            string strErr="";
+            const decimal decMaxSales = 1000000;
+            const int intMaxAdvPay = 10000, intMaxName = 20;
+            // validate input
+            if (txtLastName.Text.Length == 0 || txtLastName.Text.Length > 20)
+            {
+                blnValid = false;
+                strErr = "Last name must be at least 1 character, and no more than " + 
+                    intMaxName.ToString() + " characters long." + "\n";
+            }
+            if (decimal.TryParse(txtSales.Text, out decSales))
+            {
+                if (decSales < 0 || decSales > decMaxSales)
+                {
+                    blnValid = false;
+                    strErr += "Monthly sales must be between 0 and " + 
+                        decMaxSales.ToString() +"\n";
+                }
+            }
+            else // user entered non-decimal value
+            {
+                blnValid = false;
+                strErr += "Monthly sales must be numeric." + "\n";
+            }
+            // still need to validate advance pay...
+            if (int.TryParse(txtAdvancePay.Text, out intAdvPay))
+            {
+                if (intAdvPay < 0 || intAdvPay > intMaxAdvPay)
+                {
+                    blnValid = false;
+                    strErr += "Advance pay must be between 0 and " +
+                        intMaxAdvPay.ToString() + "\n";
+                }
+            }
+            else // user entered non-integer value
+            {
+                blnValid = false;
+                strErr += "Advance pay must be an integer.";
+            }
+            // Validation is over. If blnValid is STILL true, then continue
+            // with calculations. If blnValid is false, then only show error msg(s).
+            if (blnValid)
+            {
+                // First, determine correct commission rate
+                if (decSales < 10000)
+                {
+                    decCommissionRate = .05m;
+                }
+                else if (decSales <= 14999)
+                {
+                    decCommissionRate = .1m;
+                }
+                else if (decSales <= 17999)
+                {
+                    decCommissionRate = .12m;
+                }
+                else if (decSales <= 21999)
+                {
+                    decCommissionRate = .14m;
+                }
+                else // decSales > 21999
+                {
+                    decCommissionRate = .15m;
+                }
+                // Calculate commission and net pay
+                decCommission = decCommissionRate * decSales;
+                decNetPay = decCommission - intAdvPay;
+                // Display output
+                lblCommissionRate.Text = decCommissionRate.ToString("p");
+                lblCommission.Text = decCommission.ToString("c");
+                lblNetPay.Text = decNetPay.ToString("c");
+            }
+            else // user entered invalid data
+            {
+                // erase old output
+                lblCommissionRate.Text = "";
+                lblCommission.Text = "";
+                lblNetPay.Text = "";
+                // show error msg(s)
+                MessageBox.Show("Please correct the following: " + "\n" + strErr);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAdvancePay.Clear();
+            txtLastName.Clear();
+            txtSales.Clear();
+            lblCommission.Text = "";
+            lblCommissionRate.Text = "";
+            lblNetPay.Text = "";
+            txtLastName.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+                this.Close();
+        }
+
+        private void frmPayCalculator_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dlgReply;
+            dlgReply = MessageBox.Show("Do you want to exit?", "Confirm Exit",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dlgReply == DialogResult.No)
+            {
+                e.Cancel = true; 
+            }
+        }
+    }
+}
